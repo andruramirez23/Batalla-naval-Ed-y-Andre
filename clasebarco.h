777f033bd61ec1f9a7ebd42a05b168b65fe5bfa6
Edgomez01 <edwinandresgomez01@gmail.com>
2025-10-02 18:45:00 -0500

    Update clasebarco.h

--- clasebarco.h
+++ clasebarco.h
@@ -1,11 +1,13 @@
 #pragma once
+#include <array>
+#include <utility>
 #include <iostream>
 
-class Barco{	
-		
-	public:
-		Barco();
-		int coordenadas[2][4];
+class Barco {
+public:
+    // 4 celdas (x,y)
+    std::array<std::pair<int,int>,4> celdas{};
 
+    Barco();                       // pide por std::cin las 4 coordenadas
+    void PedirCoordenadas();       // si prefieres pedirlas luego
 };
-
